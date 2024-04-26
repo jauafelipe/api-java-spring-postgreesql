e0b9113510644480cb3ab70954f2278a145bbd29
jauã felipe <jauafelipe345@gmail.com>
2024-04-26 10:30:52 -0300

    start api

---
+++ src/main/java/com/example/apipostgre/ApiPostgreApplication.java
@@ -0,0 +1,12 @@
+package com.example.apipostgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPostgreApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPostgreApplication.class, args);
+	}
+
+}
